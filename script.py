7fa77feea05de9da2b50d363a54ad7032a616cc1
vivianhuynh91827 <vhuynh00@stuy.edu>
2020-06-11 17:30:21 -0400

    editted script to fix prism and pyramid

--- script.py
+++ script.py
@@ -212,7 +212,7 @@ def run(filename):
             elif c == 'prism':
                 if command['constants']:
                     reflect = command['constants']
-                add_cylinder(tmp, args[0], args[1], args[2], args[3], args[4], args[5])
+                add_cylinder(tmp, args[0], args[1], args[2], args[3], args[4], args[5] + 1)
                 matrix_mult( stack[-1], tmp)
                 draw_polygons(tmp, screen, zbuffer, view, ambient, light, symbols, reflect)
                 tmp = []
@@ -221,7 +221,7 @@ def run(filename):
             elif c == 'pyramid':
                 if command['constants']:
                     reflect = command['constants']
-                add_cylinder(tmp, args[0], args[1], args[2], args[3], args[4], args[5])
+                add_cylinder(tmp, args[0], args[1], args[2], args[3], args[4], args[5] + 1)
                 matrix_mult( stack[-1], tmp)
                 draw_polygons(tmp, screen, zbuffer, view, ambient, light, symbols, reflect)
                 tmp = []
